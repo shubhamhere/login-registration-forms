e8555ddbe3b19e517538e271faf53a48610227a2
Subham <paras123hzb@gmail.com>
2021-02-12 03:36:57 +0530

    adding more information

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
+require("./db/conn");
 const express = require("express");
 const app = express();
-const path = require("path")
+const path = require("path");
+const auth = require("./middleware/auth")
 const hbs = require("hbs");
-require("./db/conn");
-const Register = require("./models/registers")
-const {json}=require("express")
-const {log}=require("console")
 
+const Register = require("./models/registers")
+const { json } = require("express")
+const { log } = require("console")
+const cookieParser = require('cookie-Parser');
 const port = process.env.PORT || 3000;
 
 const bcrypt = require('bcryptjs');
@@ -19,7 +21,7 @@ const partials_path = path.join(__dirname, "../templates/partials")
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))//if not using postman
-
+app.use(cookieParser())
 app.use(express.static(static_path))
 app.set("view engine", "hbs")
 app.set("views", template_path)
@@ -30,11 +32,35 @@ console.log(process.env.SECRET_KEY);
 app.get("/", (req, res) => {
     res.render("index")
 });
+app.get("/secret", auth, (req, res) => {
+    console.log(`this is the cookie ${req.cookies.jwt}`);
+    res.render("secret")
+});
 app.get("/register", (req, res) => {
     res.render("register")
 });
 app.get("/login", (req, res) => {
     res.render("login")
+});
+app.get("/logout", auth, async (req, res) => {
+    try {
+        console.log(req.user);
+        
+        //for single logout we delete that token from database of current recent user
+        req.user.tokens = req.user.tokens.filter((currElement) => {
+            return currElement.token !== req.token
+        })
+
+//logout from all devices. ie remove all tokens from database
+req.user.tokens=[];
+//to remove that cookie on clicking logout
+        res.clearCookie("jwt")
+        console.log("loggedout sucessfully");
+        await req.user.save();
+        res.render("login")
+    } catch (error) {
+        res.status(500).send(error)
+    }
 });
 //create a new user
 app.post("/register", async (req, res) => {
@@ -54,12 +80,22 @@ app.post("/register", async (req, res) => {
 
             })
             //calling token function
- const token=await registerEmployee.generateAuthToken()
+            const token = await registerEmployee.generateAuthToken()
             console.log(`the register token is :- ${token}`);
-    
+            //the res.cookie() function is used to set cookie name to value
+            //the value parameter may be a straing or object converted to JSON
+            // res.cookie(name,value,[options])
+
+            res.cookie("jwt", token, {
+                expires: new Date(Date.now() + 10000000),
+                httpOnly: true, //now client cant do anything to our cookie
+                secure: true
+            })
+            console.log(cookie);
+
             //password hashing using bcrypt
             const registered = await registerEmployee.save();
-            
+
             res.status(201).render("index")
         } else {
             res.send("password dont match")
@@ -67,7 +103,7 @@ app.post("/register", async (req, res) => {
     } catch (error) {
         res.status(400).send(error)
         console.log(`the error part is ${error}`);
-        
+
     }
 });
 
@@ -80,10 +116,17 @@ app.post("/login", async (req, res) => {
         const useremail = await Register.findOne({ email: email })
 
         const isMatch = await bcrypt.compare(password, useremail.password);//returns true or false
-  
-        const token=await useremail.generateAuthToken()
-        
+
+        const token = await useremail.generateAuthToken()
+
         console.log(`the login token is :- ${token}`);
+
+        res.cookie("jwt", token, {
+            expires: new Date(Date.now() + 10000000),
+            httpOnly: true, //now client cant do anything to our cookie
+            // secure:true
+        })
+
 
         if (isMatch) {
 
@@ -110,7 +153,7 @@ app.post("/login", async (req, res) => {
 const jwt = require('jsonwebtoken');
 
 const createToken = async () => {
-    const token = await jwt.sign({ _id: "6024591a507e3a33c00368d4" }, "hsbdhdbdbshnsnsxxnsjdsopqakslmanassnssjkuhssjss", )
+    const token = await jwt.sign({ _id: "6024591a507e3a33c00368d4" }, "hsbdhdbdbshnsnsxxnsjdsopqakslmanassnssjkuhssjss",)
     console.log(token);
     const useVer = await jwt.verify(token, "hsbdhdbdbshnsnsxxnsjdsopqakslmanassnssjkuhssjss")
     console.log(useVer);
---
+++ src/middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const Register = require('../models/registers');
+
+const auth = async (req,res,next)=>{ //next is used to call next user
+   try {
+       const token = req.cookies.jwt;
+       const verifyUser=jwt.verify(token, process.env.SECRET_KEY);
+       console.log(verifyUser);
+       const user= await Register.findOne({_id:verifyUser._id})
+       console.log(user.firstname);
+       
+       req.token=token;
+       req.user=user;
+       
+       next();
+   } catch (error) {
+       res.status(401).send(error)
+   }
+}
+module.exports=auth;
